f95cef01fa0de19a648d151d5d10a9669b5f82b5
Misael06 <tochipotter@gmail.com>
2020-09-26 02:22:53 +0900

    ユーザー登録のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name, uniqueness: true
+    validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :firstname, { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :lastname_jp, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :firstname_jp, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :birthday
+  end
+
 end
