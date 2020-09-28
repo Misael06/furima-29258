87e85a71eef799b291bea33fa6df8154bfa08e1d
Misael06 <tochipotter@gmail.com>
2020-09-28 20:04:54 +0900

    itemテーブル作成

--- db/migrate/20200928094648_create_items.rb
+++ db/migrate/20200928094648_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name,            null: false
+      t.text :explanatory,       null: false
+      t.integer :price,          null: false
+      t.integer :category,       null: false
+      t.integer :status,         null: false
+      t.integer :fee,            null: false
+      t.integer :shipmentsource, null: false
+      t.integer :timerequired,   null: false
+      t.references :user,        foreign_key: true
       t.timestamps
     end
   end
