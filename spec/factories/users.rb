bf1f237a43ee229c0d05358588a7059c33dfae3f
Misael06 <tochipotter@gmail.com>
2020-09-28 17:49:21 +0900

    結合テストコード作成完了

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     email                 {"kkk@gmail.com"}
     password              {"aaaa00"}
     password_confirmation {password}
-    lastname              {"松崎"}
-    firstname             {"聡"}
+    firstname             {"松崎"}
+    lastname              {"聡"}
     lastname_jp           {"マツザキ"}
     firstname_jp          {"サトシ"}
     birthday              {"2000/01/01"}
