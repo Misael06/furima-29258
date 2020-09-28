ccb7a05e9471a0f742974877dd7011776a175c61
Misael06 <tochipotter@gmail.com>
2020-09-28 18:37:17 +0900

    単体テストコードの簡潔化

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,38 +8,8 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     context '新規登録がうまくいくとき' do
       it "nicknameとemail、passwordとpassword_confirmation,lastnameとfirstname,lastname_jpとfirstname_jp,birthdayが存在すれば登録できる" do
-        user = FactoryBot.build(:user)
+        @user = FactoryBot.build(:user)
         expect(@user).to be_valid
-      end
-      it "nicknameが入力されてたらで登録できる" do
-        @user.nickname = "jona"
-        expect(@user).to be_valid
-      end
-      it "passwordが英数字を含む6文字以上であれば登録できる" do
-        @user.password = "aaaa00"
-        @user.password_confirmation = "aaaa00"
-        expect(@user).to be_valid
-      end
-      it "lastnameが日本語で入力されていれば登録できる" do
-        @user.lastname = "松崎"
-        expect(@user).to be_valid
-      end
-      it "firstnameが日本語で入力されていれば登録できる" do
-        @user.firstname = "聡"
-        expect(@user).to be_valid
-      end
-      it "lastname_jpがカタカナで入力されていれば登録できる" do
-        @user.lastname_jp = "マツザキ"
-        expect(@user).to be_valid
-      end
-      it "firstname_jpがカタカナで入力されていれば登録できる" do
-        @user.firstname_jp = "サトシ"
-        expect(@user).to be_valid
-      end
-      it "birthdayが入力されていれば登録できる" do
-        @user.birthday = "2000/01/01"
-        expect(@user).to be_valid
-      end
     end
 
     context '新規登録がうまくいかないとき' do
