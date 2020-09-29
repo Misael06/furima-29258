3ffca9af933fb1be2678e96c72c114228a9df44b
Misael06 <tochipotter@gmail.com>
2020-09-29 19:10:52 +0900

    Active_Hashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,14 +2,32 @@ class ItemsController < ApplicationController
   before_action :move_to_new, except: [:index]
 
   def index
+    @items =Item.all
   end
 
   def new
+    @item =Item.create
+  end
+
+  def create
+    @item =Item.create(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
   end
 
   def move_to_new
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_image, :name, :explanatory, :price, :category, :status, :fee, :shipmentsource, :timerequired).merge(user_id: current_user.id)
   end
 end
