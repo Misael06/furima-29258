74ccb472feab1e71f6a0fe74ce8fdcf15cab1d64
Misael06 <tochipotter@gmail.com>
2020-09-29 19:09:30 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,19 @@
 class Item < ApplicationRecord
+  has_one_attached :item_image
+  belongs_to :user
+
+  with_options presence: true do
+    validates :item_image
+    validates :name
+    validates :explanatory
+    validates :price, format: {with: /\A[0-9]+\z/, message: "Half-width number"}, 
+              :numericality => { greater_than_or_equal_to: 300, less_than: 10000000, message:"Out of setting range"} 
+  end
+  with_options exclusion: { in: %w(---),message: "Select" } do
+    validates :category
+    validates :status
+    validates :fee
+    validates :shipmentsource
+    validates :timerequired
+  end
 end
