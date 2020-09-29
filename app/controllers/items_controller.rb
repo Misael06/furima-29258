215aef43b79f73849807c2b6567bf1d2033471e9
Misael06 <tochipotter@gmail.com>
2020-09-29 11:21:14 +0900

    出品画面への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :move_to_new, except: [:index]
+
   def index
+  end
+
+  def new
+  end
+
+  def move_to_new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
