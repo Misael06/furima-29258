393ca4cc210935e8428cc47a445954f91457455d
Misael06 <tochipotter@gmail.com>
2020-10-01 10:22:14 +0900

    商品編集コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,26 @@ class ItemsController < ApplicationController
     @item =Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    if item.valid?
+      item.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+  #商品削除で確認
+  def destroy
+  end
+  #/商品削除で再度確認
+
   def move_to_new
     unless user_signed_in?
       redirect_to new_user_session_path
