93efa6849bb416f56cc3f990511bc9e549b8e4f2
Misael06 <tochipotter@gmail.com>
2020-10-01 17:02:05 +0900

    itemコントローラーの調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_new, except: [:index]
+  before_action :move_to_new, except: [:index, :show]
 
   def index
     @items= Item.all.order(created_at: :desc)
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item =Item.create(item_params)
+    @item =Item.new(item_params)
     if @item.valid?
       @item.save
       redirect_to root_path
