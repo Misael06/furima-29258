c4f2d6c6db604abfd2e48800830a5cbf3f4d072d
Misael06 <tochipotter@gmail.com>
2020-10-06 11:13:08 +0900

    電話番号のバリデーション変更

--- app/forms/order_send_place.rb
+++ app/forms/order_send_place.rb
@@ -8,7 +8,7 @@ class OrderSendPlace
     validates :postalcode, format: { with: /\A\d{3}[-]\d{4}\z/}
     validates :municipality
     validates :homeadress
-    validates :phonenumber, format: { with: /\A\d{,11}\z/}
+    validates :phonenumber, format: { with: /\A\d{10,11}\z/}
   end
 
   validates :prefecture_id, numericality: { other_than: 0, message: "Select" }
