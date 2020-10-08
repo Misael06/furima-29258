6483b5991836a4e06147277f14d483fac9cae2b3
Misael06 <tochipotter@gmail.com>
2020-10-08 22:25:24 +0900

    Cardのモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :items
   has_many :orders
+  has_one :card, dependent: :destroy
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/
   with_options presence: true do
