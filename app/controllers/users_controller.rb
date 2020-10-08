63963c263348efb12b4056fa3c7b5b681434ef3a
Misael06 <tochipotter@gmail.com>
2020-10-08 23:29:50 +0900

    userコントローラーの調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def show
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
     card = Card.find_by(user_id: current_user.id) # ユーザーのid情報を元に、カード情報を取得
@@ -7,5 +8,19 @@ class UsersController < ApplicationController
 
     customer = Payjp::Customer.retrieve(card.customer_token) # 先程のカード情報を元に、顧客情報を取得
     @card = customer.cards.first
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      redirect_to "show"
+    end
+  end
+ 
+  private
+ 
+  def user_params
+    params.require(:user).permit(:nickname, :email)
   end
 end
